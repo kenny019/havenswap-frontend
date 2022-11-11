c60bff5465f330293e27c45c5f3e66d48ba9822e
kenny019 <ngkenny019@gmail.com>
2022-11-11 17:30:33 +0800

    fix bug with removing selected nfts

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -7,6 +7,15 @@ import {
 
 import { useState } from 'react';
 
+function removeItemOnce(arr, value) {
+	var index = arr.indexOf(value);
+	if (index > -1) {
+		arr.splice(index, 1);
+	}
+	return arr;
+}
+
+
 const SkeletonImages = () => {
 	return (
 		<Skeleton
